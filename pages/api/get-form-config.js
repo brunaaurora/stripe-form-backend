65408715224d5387aa2ffdf62ed903d2b0072fc7
brunaaurora <thevisuallaboratory@gmail.com>
2025-05-14 00:31:15 +0300

    Update get-form-config.js

--- pages/api/get-form-config.js
+++ pages/api/get-form-config.js
@@ -1,6 +1,17 @@
 import { google } from 'googleapis';
 
 export default async function handler(req, res) {
+  // Add CORS headers
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  // Handle preflight request
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+
   if (req.method !== 'GET') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
